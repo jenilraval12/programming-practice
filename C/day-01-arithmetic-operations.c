9f5e5d8c71357ecb5d4bc1ac025d7fb8b0b5edd4
Jenil Raval <ravaljenil6@gmail.com>
2026-08-06 23:13:55 +0530

    Wrote the program and implemented division by zero check in arithmetic operations
    Wrote the code and added error handling for division by zero in arithmetic operations.

--- C/day-01-arithmetic-operations.c
+++ C/day-01-arithmetic-operations.c
@@ -13,4 +13,38 @@ Input
 Two integers entered by the user.
 
 Output
-Display all five arithmetic operations.*/
+Display all five arithmetic operations.
+
+Bonus Challenge ⭐
+
+Handle the case when the second number is 0.
+Instead of crashing or causing undefined behavior, print:
+
+"Division by zero is not possible."*/
+
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+    printf("Enter First Integer Number: ");
+    scanf("%d", &num1);
+    printf("Enter Second Integer Number: ");
+    scanf("%d", &num2);
+
+    if (num2 == 0)
+    {
+        printf("Error: Division by zero is not possible");
+    }
+    else
+    {   
+        printf("\n=========================");
+        printf("\nArithmetic Operations");
+        printf("\n=========================");
+        printf("\n\nSum of %d and %d is: %d", num1, num2, (num1 + num2));
+        printf("\nDifference of %d and %d is: %d", num1, num2, (num1 - num2));
+        printf("\nProduct of %d and %d is: %d", num1, num2, (num1 * num2));
+        printf("\nQuotient of %d and %d is: %d", num1, num2, (num1 / num2));
+        printf("\nRemainder of %d and %d is: %d", num1, num2, (num1 % num2));
+    }
+}
