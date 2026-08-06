5d6552171028d32d33f2f8a347023811dec002ba
Jenil Raval <ravaljenil6@gmail.com>
2026-08-06 22:47:04 +0530

    Create day-01-arithmetic-operations.c
    Day 1: Arithmetic operations in C

---
+++ C/day-01-arithmetic-operations.c
@@ -0,0 +1,16 @@
+/*Day 1 – Problem 1
+Problem Statement
+
+Write a C program to input two integers from the user and display:
+
+1) Sum
+2) Difference
+3) Product
+4) Quotient
+5) Remainder
+
+Input
+Two integers entered by the user.
+
+Output
+Display all five arithmetic operations.*/
