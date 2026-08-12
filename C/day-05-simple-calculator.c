c0e7cf22b10760713f5cafadeec15d84eb00c921
Jenil Raval <ravaljenil6@gmail.com>
2026-08-12 20:07:45 +0530

    Created C file and Wrote Problem Statement for Simple Calculator Using C Language

---
+++ C/day-05-simple-calculator.c
@@ -0,0 +1,42 @@
+/*Problem Statement
+
+Write a C program that accepts two numbers and an arithmetic operator from the user, then performs the selected operation.
+
+The calculator should support:
+
+Addition +
+Subtraction -
+Multiplication *
+Division /
+Remainder %
+
+
+Bonus Challenge ⭐
+
+Allow the user to perform the calculation using floating-point numbers so that something like:
+
+10 / 4
+
+produces:
+Result: 2.50
+
+You'll need to think about whether int is still the appropriate data type.
+
+Extra Challenge ⭐⭐
+
+Create a menu-based calculator:
+
+========================
+    CALCULATOR
+========================
+
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
+5. Remainder
+
+Enter your choice:
+
+Then use switch based on the user's choice number rather than directly switching on +, -, etc.*/
+
