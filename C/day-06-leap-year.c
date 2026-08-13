29e2872599d90600ee7bd1dae947373611e07df4
Jenil Raval <ravaljenil6@gmail.com>
2026-08-13 22:31:51 +0530

    Day-6 Problem stated and file created

---
+++ C/day-06-leap-year.c
@@ -0,0 +1,34 @@
+/*Day 6 - Leap Year Checker
+
+Problem Statement
+Write a C program that accepts a year from the user and determines
+whether the given year is a leap year or not.
+
+Leap Year Rules
+A year is considered a leap year if:
+- It is divisible by 400, OR
+- It is divisible by 4 but not divisible by 100.
+
+Requirements
+- Accept a year as an integer using scanf().
+- Use the modulus (%) operator to check divisibility.
+- Use if-else statements and logical operators to implement the
+  leap year conditions.
+- Display whether the entered year is a leap year or not.
+- Handle invalid input such as zero or negative years.
+
+Examples
+Input:
+2024
+
+Output:
+2024 is a Leap Year.
+
+
+Input:
+1900
+Output:
+1900 is not a Leap Year.
+
+The program should correctly handle leap years, non-leap years,
+century years, and invalid year values.*/
